3343d16e36687a394a81598dbe7f39e70a82e2ce
Suraj <suraj.kedari@cctech.co.in>
2022-03-03 16:31:03 +0530

    Trial commit

---
+++ SK_First_trial_commit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+     int a, b,c;
+     void add();
+     {
+         cout<<"a and b are: "<<endl;
+         cin>>a>>b;
+          c= a+b;
+
+     }
+     cout<<"addition of a and b is= "<<c<<endl;
+    return 0;
+}
